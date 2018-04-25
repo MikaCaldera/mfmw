c0bf15c82e1b8b0c22801b3d03f565fa66c696db
MikaCaldera <mcaldera@mirandacreative.com>
2018-04-25 10:37:18 -0400

    title edits

--- wp-content/themes/march-farm/header.php
+++ wp-content/themes/march-farm/header.php
@@ -1,18 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Starter Site For March Farm">
+    <!-- Mobile Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content="March Farm">
     <meta name="author" content="Mika Caldera">
-    <link rel="shortcut icon" href="<?php echo THEME_DIR; ?>/path/favicon.ico" />
-    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS2 Feed" href="<?php bloginfo('rss2_url'); ?>" />
+
+    <!-- Icons & Favicons -->
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+    <link href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png" rel="apple-touch-icon" />
+
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-    <title><?php wp_title(); ?> - <?php bloginfo( 'name' ); ?></title>
+
+    <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+    
     <?php wp_head(); ?>
     <?php the_field('header_scripts', 'option'); ?>
   </head>
    <body class="">
     <?php include "inc/hero.php" ?>
     <?php include "inc/nav.php" ?>
+
+
+
+
+        
