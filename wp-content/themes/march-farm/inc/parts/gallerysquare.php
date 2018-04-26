ddef18ce409196c91460ab570b109d5804aabf8c
MikaCaldera <mcaldera@mirandacreative.com>
2018-04-25 23:02:05 -0400

    adding buttons to acf in all galleies

--- wp-content/themes/march-farm/inc/parts/gallerysquare.php
+++ wp-content/themes/march-farm/inc/parts/gallerysquare.php
@@ -18,9 +18,9 @@
       </div>
     </div>
   </div>
-      <div class="row justify-content-md-center">
-          <a href="<?php the_sub_field('link_url'); ?>">
-            <button type="button" class="btn btn-light btn-lg"> Button<?php the_sub_field('link_text'); ?></button>
+       <div class="row justify-content-md-center">
+          <a href="<?php the_sub_field('button_link'); ?>">
+            <button type="button" class="btn btn-light btn-lg"><?php the_sub_field('button_text'); ?></button>
           </a>
         </div>
 </section>
