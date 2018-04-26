b8591d00e0dbc1e0f4ab84e47feca4e45717ef56
pdxor <admin@mirandacreative.com>
2018-04-26 10:08:41 -0400

    removed map from subpages and moved loop to below flex

--- wp-content/themes/march-farm/front-page.php
+++ wp-content/themes/march-farm/front-page.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
+        <?php include "inc/flex.php" ?>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <?php the_content(); ?>
         <?php endwhile; else: ?>
             <?php _e( 'Sorry, no pages matched your criteria.', 'textdomain' ); ?>
-        <?php endif; ?>
-        <?php include "inc/flex.php" ?>
+        <?php endif; ?>        
         <?php include "inc/parts/map.php" ?>
         <?php include "inc/megafooter.php" ?>
         <?php include "inc/footer.php" ?>
--- wp-content/themes/march-farm/fullwidth-page.php
+++ wp-content/themes/march-farm/fullwidth-page.php
@@ -4,13 +4,12 @@ Template Name: fulwidth-page
 Template Post Type: page, post
 */
 get_header(); ?>
+      <?php include "inc/flex.php" ?>
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <?php the_content(); ?>
       <?php endwhile; else: ?>
           <?php _e( 'Sorry, no pages matched your criteria.', 'textdomain' ); ?>
-      <?php endif; ?>
-      <?php include "inc/flex.php" ?>
-      <?php include "inc/parts/map.php" ?>
+      <?php endif; ?>      
       <?php include "inc/megafooter.php" ?>
       <?php include "inc/footer.php" ?>
   </body>
