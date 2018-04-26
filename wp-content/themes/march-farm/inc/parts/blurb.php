92e3fc830696e687a1c4fd46d5e52e9af22688f5
pdxor <admin@mirandacreative.com>
2018-04-26 09:30:04 -0400

    blurb  fixed

--- wp-content/themes/march-farm/inc/parts/blurb.php
+++ wp-content/themes/march-farm/inc/parts/blurb.php
@@ -1,19 +1,19 @@
 <!-- Blurb Section -->
 <div class="gray-section">
- <?php if (have_rows('blurb')):?>  
+ <?php if (have_rows('blurb')):?>
   <section class="container blurbhold" >
-        <?php while (have_rows('blurb')) : the_row(); 
+        <?php while (have_rows('blurb')) : the_row();
             $title = get_sub_field('title');
             $content = get_sub_field('content');
             $picture = get_sub_field('picture');
-        ?>    
+        ?>
     <div class="row">
       <div class="col-lg-6">
         <h1 class="blurb-header"><?= $title; ?></h1>
         <p><?= $content; ?></p>
       </div>
       <div class="col-lg-6">
-        <img class="" src="<?= $picture; ?>" alt="">
+        <img class="picture" src="<?= $picture; ?>" alt="">
       </div>
     </div>
         <?php endwhile; ?>
