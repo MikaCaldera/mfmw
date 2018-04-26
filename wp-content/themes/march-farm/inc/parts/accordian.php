3ea42b190f6431bb5ee3ce8954e87e2c7824430b
pdxor <admin@mirandacreative.com>
2018-04-26 11:49:35 -0400

    Added random number that switches for every accordian instance.

--- wp-content/themes/march-farm/inc/parts/accordian.php
+++ wp-content/themes/march-farm/inc/parts/accordian.php
@@ -1,5 +1,6 @@
 <?php if (have_rows('accordian_tab')):?>
   <?php   $i = 0; ?>
+  <?php $random = rand(1,222); ?>
   <!-- Accordian Section -->
   <div class="accordion">
     <section class="container" >
@@ -9,17 +10,18 @@
              $headline = get_sub_field('headline');
              $content = get_sub_field('content');
              $i++;
+             $random++;
              ?>
                   <div id="accordion">
                     <div class="">
-                      <div class="card-title" id="heading<?= $i ?>">
+                      <div class="card-title" id="heading<?= $i ?>-<?= $random ?>">
                         <h5 class="mb-0">
-                          <button class="btn btn-link" data-toggle="collapse" data-target="#collapse<?= $i ?>" aria-expanded="true" aria-controls="collapse<?= $i ?>">
+                          <button class="btn btn-link" data-toggle="collapse" data-target="#collapse<?= $i ?>-<?= $random ?>" aria-expanded="true" aria-controls="collapse<?= $i ?>-<?= $random ?>">
                             <?= $headline ?>
                           </button>
                         </h5>
                       </div>
-                      <div id="collapse<?= $i ?>" class="collapse <?php if($i == 1){ echo 'show'; }?>" aria-labelledby="heading<?= $i ?>" data-parent="#accordion">
+                      <div id="collapse<?= $i ?>-<?= $random ?>" class="collapse <?php if($i == 1){ echo 'show'; }?>" aria-labelledby="heading<?= $i ?>-<?= $random ?>" data-parent="#accordion">
                         <div class="card-body">
                           <?= $content ?>
                       </div>
