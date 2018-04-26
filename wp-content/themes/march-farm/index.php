7a983b892d50c9a7e9666d2444f7356d314a8f87
pdxor <admin@mirandacreative.com>
2018-04-26 09:53:20 -0400

    removed edit link

--- wp-content/themes/march-farm/index.php
+++ wp-content/themes/march-farm/index.php
@@ -2,7 +2,6 @@
 get_header(); ?>
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <?php the_content(); ?>
-          <?php edit_post_link(); ?>
       <?php endwhile; else: ?>
           <?php _e( 'Sorry, no pages matched your criteria.', 'textdomain' ); ?>
       <?php endif; ?>
