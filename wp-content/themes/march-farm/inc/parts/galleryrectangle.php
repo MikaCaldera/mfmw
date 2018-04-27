11ada81cc194b55203e32e0500ac17f20da98fcf
pdxor <admin@mirandacreative.com>
2018-04-27 14:58:40 -0400

    Added button logic

--- wp-content/themes/march-farm/inc/parts/galleryrectangle.php
+++ wp-content/themes/march-farm/inc/parts/galleryrectangle.php
@@ -1,4 +1,32 @@
+    <?php
 
+    $type_of_link = get_sub_field('type_of_link');
+
+    if ($type_of_link == "page")
+    {
+      $button_link = get_sub_field('page_link');
+      $target = '_self';
+    }
+    elseif ($type_of_link == "url")
+    {
+      $button_link = get_sub_field('url_link');;
+      $target = '_blank';
+    }
+    elseif ($type_of_link == "file")
+    {
+      $button_link = get_sub_field('file_link');
+      $target = '_blank';
+    }
+    elseif ($type_of_link == "anchor")
+    {
+      $button_link = '#'. get_sub_field('anchor_link');
+      $target = '_self';
+    }
+    else
+    {
+
+    }
+    ?>
 <!-- Gallery Rectangle Section -->
  <section class="gallery" >
    <div class="row">
@@ -21,10 +49,12 @@
        </div>
      </div>
    </div>
-      <div class="row justify-content-md-center">
-        <a href="<?php the_sub_field('button_link'); ?>">
-          <button type="button" class="btn btn-light btn-lg"><?php the_sub_field('button_text'); ?></button>
-        </a>
-      </div>
+   <?php if( !empty($buttontext) ): ?>
+   <div class="row justify-content-md-center">
+         <a target="<?= $target; ?>" href="<?= $button_link; ?>">
+           <button type="button" class="btn btn-light btn-lg"><?= $buttontext ?></button>
+         </a>
+   </div>
+   <?php endif; ?>
  </section>
  <!-- /Gallery -->
